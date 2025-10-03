7217200b1b4b4dac74b21094a0bbb38ff44c3cb6
flower <nandhini.569@gmail.com>
2025-10-03 22:10:16 +0530

    folder structure created sucessfully

---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch research/trials.ipynb
+touch app.py
+touch .env
+touch requirements.txt
+
+echo "directory structure created successfully"
